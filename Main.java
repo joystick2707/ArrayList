5b60f825ac4925763ce3a2728ac85ea970dc951d
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-04-15 15:48:18 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        MeuArrayList lista = new MeuArrayList(2);
+        lista.adicionar("Bryan");
+        lista.adicionar("Lucas");
+        lista.adicionar("Paulo");
+
+        System.out.println("Elementos:");
+        lista.imprimir();
+
+        lista.remover(1);
+
+        System.out.println("\nDepois de remover o índice 1:");
+        lista.imprimir();
+    }
+}
